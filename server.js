e297b136a844c8a41bf51bbefbf5a89e73867f49
ruida shen <shenruida@gmail.com>
2020-05-16 22:57:47 -0700

    change server port

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ nunjucks.configure("view", { autoescape: true, express: app });
 
 app.use(express.static("public"));
 app.use(router);
-
-app.listen(5000);
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'));
